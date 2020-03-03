0c885feb14c6feeae00f311ca2156cab89ddbd3b
rainbow8881 <mafmaf8881@gmail.com>
2020-03-04 03:27:01 +0800

    final

---
+++ src/FinalTest/FinalTest01.java
@@ -0,0 +1,20 @@
+package FinalTest;
+
+/**
+ * @outhor Rainbow
+ * @date 2020/2/29 - 5:46 下午
+ *  Final 是一个关键字 表示最终的 不可变的
+ *        修饰的类 无法被继承
+ *        修饰的方法无法被覆盖
+ *        修饰的变量一旦被赋值之后  不可重新赋值
+ *        修饰的实例变量？？？？？？
+ *        修饰的引用？？？？？
+ *
+ *
+ *
+ */
+public class FinalTest01 {
+    public static void main(String[] args) {
+
+    }
+}
